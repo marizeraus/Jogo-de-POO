eecaa7292daeb77592bc87bb201980f55edc06b6
Jonathan Meirelles <33517543+jmsgfhr@users.noreply.github.com>
2019-06-01 16:29:50 -0300

    Add files via upload

---
+++ Asteroids.java
@@ -0,0 +1,21 @@
+
+package asteroids;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.StateBasedGame;
+/**
+ * quando iniciarem o projeto vá em propriedades do projeto e alterem o local do slick
+ * @author jmsgfhr,(add o nome de vcs)
+ */
+public class Asteroids extends StateBasedGame{
+
+    public static final String gamename = "Asteroids";
+    public static final int menu = 0;
+    public static final int play = 1;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
